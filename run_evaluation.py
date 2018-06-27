ed2486bf247396182e4baca86c624a4b8fdaaaee
ingridavh <ingridavh@hotmail.com>
2018-06-27 17:29:36 +0200

    Added make-file or run-file or whatever we call it

---
+++ run_evaluation.py
@@ -0,0 +1,17 @@
+"""
+Run an instance of the evaluation.py program
+with slha-file as input
+
+@author: Ingrid A V Holm
+"""
+
+import evaluation_as_module as eam
+
+eam.xsections = [(1000021, 1000001), (1000021, 1000003)]
+eam.xsections = [(1000001, -1000001), (1000001, 1000002)]
+eam.xsections = [(1000021, 1000021)]
+a = eam.get_type(eam.xsections)
+print 'See here', a
+
+#print a[(1000001, 1000002)] # Getting out the production channel type
+eam.eval_xsection(1000, 500, 600, 500, 600, 500, 500, 500, 500)
