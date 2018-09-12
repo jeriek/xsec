fd6e546cf281168ecbfb49ea004be9e2609c57b9
jeriek <jeriekvda@fys.uio.no>
2018-09-12 17:51:15 +0200

    Make timing output nicer

--- timing.py
+++ timing.py
@@ -6,47 +6,57 @@ import numpy as np
 import evaluation as eval
 
 
-
 def main(): 
 	ntimes = 1000
 
-	# eval.xsections = [(1000021, 1000001), (1000021, 1000003)]
-	# eval.xsections = [(1000001, -1000001), (1000001, 1000002)]
 	eval.xsections = [(1000021, 1000021)]
+	# eval.DATA_DIR = './data/1000pts/'
+	eval.DATA_DIR = './data/5000pts/'
 	start_parameters = (1000, 500, 600, 500, 600, 500, 500, 500, 500)
-	eval.init(use_cache=False, cache_dir="$SCRATCH/xsec_cache", flush_cache=False,\
-		 use_memmap=False)
+	
+	eval.init() # run with default settings (no caching)
+	# eval.init(use_cache=False, cache_dir="$SCRATCH/xsec_cache", flush_cache=False,\
+	# 	 use_memmap=False)
+
+	START_BENCHMARK = True
+	# START_BENCHMARK = False
 
 	# Start occupying all requested cores 
 	# ---------------------------
-	size = 5000
-	A, B = np.random.random((size, size)), np.random.random((size, size))
-	# Matrix multiplication
-	N = 10
-	t_wall = time.time()
-	t_CPU = time.clock()
-	for i in range(N):
-	    np.dot(A, B)
-	delta_wall = time.time() - t_wall
-	delta_CPU = time.clock() - t_CPU
-	print('Dotted two %dx%d matrices in %0.2f s. (wall time)' % (size, size, delta_wall / N))
-	print('Dotted two %dx%d matrices in %0.2f s. (CPU time)' % (size, size, delta_CPU / N))
-	del A, B
+	if START_BENCHMARK:
+		print "*** Warming up with a Numpy benchmark test ***"
+		size = 5000
+		A, B = np.random.random((size, size)), np.random.random((size, size))
+		# Matrix multiplication
+		N = 10
+		t_wall = time.time()
+		t_CPU = time.clock()
+		for i in range(N):
+		    np.dot(A, B)
+		delta_wall = time.time() - t_wall
+		delta_CPU = time.clock() - t_CPU
+		print('Dotted two %dx%d matrices in %0.2f s. \
+			(wall time)' % (size, size, delta_wall / N))
+		print('Dotted two %dx%d matrices in %0.2f s. \
+		 	(CPU time)' % (size, size, delta_CPU / N))
+		del A, B
 	# ---------------------------
 	
 	# Run actual cross-section evaluation code
 	# ---------------------------
-	blockPrint()
+	print "*** Loading GP models ***"
 	TOTAL_UNPICKLE_TIME, TOTAL_K_LOAD_TIME, TOTAL_LOAD_TIME = \
 		eval.load_processes(eval.xsections)
 
+	print "*** Running xsection evaluation, ", ntimes, " times ***"
 	avg_TOTAL_EVAL_SETUP_TIME, avg_TOTAL_EVAL_COMP_TIME, \
 		avg_TOTAL_GP_COMP_TIME, avg_TOTAL_EVAL_TIME = \
 		timing(eval.eval_xsection, ntimes, start_parameters)
 	# ---------------------------	
 
-	ndec = 5
-	enablePrint()
+	ndec = 5 # precision 1e-5
+
+	print "*** Timing results ***"
 	print "TOTAL_UNPICKLE_TIME = ", round(TOTAL_UNPICKLE_TIME, ndec) 
 	print "TOTAL_K_LOAD_TIME = ", round(TOTAL_K_LOAD_TIME, ndec) 
 	print "TOTAL_LOAD_TIME = ", round(TOTAL_LOAD_TIME, ndec) 
@@ -95,13 +105,13 @@ def timing(f, n, arg):
 	return avg_TOTAL_EVAL_SETUP_TIME, avg_TOTAL_EVAL_COMP_TIME, \
 		avg_TOTAL_GP_COMP_TIME, avg_TOTAL_EVAL_TIME
 
-# Disable printing
-def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
+# # Disable printing
+# def blockPrint():
+#     sys.stdout = open(os.devnull, 'w')
 
-# Restore printing
-def enablePrint():
-    sys.stdout = sys.__stdout__
+# # Restore printing
+# def enablePrint():
+#     sys.stdout = sys.__stdout__
 
 if __name__ == '__main__':
 	main()
