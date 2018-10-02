30c4daa4b0b887588c50605c5900233d570c8392
jeriek <jeriekvda@fys.uio.no>
2018-10-02 20:52:59 +0200

    Large restructuring, code cleanup and documentation, output now works for multiple xsections simultaneously

--- run_evaluation.py
+++ run_evaluation.py
@@ -7,13 +7,13 @@ Run an instance of the evaluation.py program
 import evaluation as evl
 
 # *** Set processes to load ***
-# evl.xsections = [(1000021, 1000021)]
+# evl.XSECTIONS = [(1000021, 1000021)]
 evl.XSECTIONS = [(1000001, -1000001), (1000001, 1000002),
                  (1000021, 1000001), (1000021, 1000003)]
-# evl.xsections = [(1000001, -1000001), (1000001, 1000002)]
+# evl.XSECTIONS = [(1000001, -1000001), (1000001, 1000002)]
 
 # *** Set directory with trained GP models (checks ./data if not specified here) ***
-evl.DATA_DIR = '../../Nimbus/NIMBUS/gps/'
+evl.DATA_DIR = '../../Nimbus/NIMBUS/gps'
 # evl.DATA_DIR = './data/1000pts'
 # evl.DATA_DIR = './data'
 
