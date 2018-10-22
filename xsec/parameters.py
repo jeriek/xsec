d7d090c8d837fa6f3af789caf32598a06afd6cb1
ahye <ahye@fys.uio.no>
2018-10-22 17:41:20 +0100

    Added SLHA interface through pySLHA. Code now depends on pySLHA.

--- xsec/parameters.py
+++ xsec/parameters.py
@@ -1,4 +1,6 @@
-# Module containing dictionary of parameters
+# Module containing dictionary of parameters and input output methods
+
+import pyslha
 
 PARAMS = {
     'm1000001': None,
@@ -23,10 +25,6 @@ MEAN_INDEX = ['m1000004', 'm1000003', 'm1000001', 'm1000002',
               'm2000002', 'm2000001', 'm2000003', 'm2000004']
 
 
-# Function to import parameters from SLHA-file
-def import_slha(filename):
-    print 'Not implemented!'
-
 # Set single parameter with key name to value
 # TODO try/except
 def set_parameter(name, value):
@@ -46,6 +44,45 @@ def set_mean_mass():
     PARAMS['mean'] = m
     return m
 
-# set_parameter('rar',700)
-# set_parameters({'m1000001':700,'m1000001':600})
-#print PARAMS['m1000001'], PARAMS['m1000002']
+
+################################
+# SLHA1 interface using pySLHA #
+################################
+
+# Import parameters from SLHA-file. This also calculates a mean squark mass
+def import_slha(filename):
+    # Try to open file
+    try:
+        slha = pyslha.read(filename, ignoreblocks=['DCINFO']) # TODO: More checking of reasonable file?
+    except IOError as e:
+        print 'Unable to find SLHA file %s. Parameters not set.' % filename
+        raise e
+
+    # Find masses
+    PARAMS['m1000001'] = slha.blocks['MASS'][1000001]
+    PARAMS['m1000002'] = slha.blocks['MASS'][1000002]
+    PARAMS['m1000003'] = slha.blocks['MASS'][1000003]
+    PARAMS['m1000004'] = slha.blocks['MASS'][1000004]
+    PARAMS['m1000005'] = slha.blocks['MASS'][1000005]
+    PARAMS['m1000006'] = slha.blocks['MASS'][1000006]
+    PARAMS['m2000001'] = slha.blocks['MASS'][2000001]
+    PARAMS['m2000002'] = slha.blocks['MASS'][2000002]
+    PARAMS['m2000003'] = slha.blocks['MASS'][2000003]
+    PARAMS['m2000004'] = slha.blocks['MASS'][2000004]
+    PARAMS['m2000005'] = slha.blocks['MASS'][2000005]
+    PARAMS['m2000006'] = slha.blocks['MASS'][2000006]
+    PARAMS['m1000021'] = slha.blocks['MASS'][1000021]
+    # Also calculate mean squark mass
+    set_mean_mass()
+    # Find mixing angles
+    PARAMS['thetab'] = slha.blocks['SBOTMIX'][1,1]
+    PARAMS['thetat'] = slha.blocks['STOPMIX'][1,1]
+
+# Write calculated cross sections to SLHA file in XSECTION block
+def write_slha(filename):
+    print 'Not implemented!'
+
+
+#import_slha('sps2a.slha')
+#for key in PARAMS:
+#    print key, PARAMS[key]
