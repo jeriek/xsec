46de87ee30b1aadf0f570497a4c1010487d39cd3
jeriek <jeriekvda@fys.uio.no>
2018-10-26 16:08:44 +0200

    Define easily accessible public interface functions

---
+++ xsec/__init__.py
@@ -0,0 +1,11 @@
+from gploader import (
+    init, set_processes, load_processes, get_processes, clear_cache
+    )
+from parameters import (
+    set_parameter, set_parameters, set_common_squark_mass,
+    set_gluino_mass, clear_parameter, clear_parameters,
+    get_parameter, get_parameters, import_slha
+    )
+from evaluation import (
+    eval_xsection
+    )
